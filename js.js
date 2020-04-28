80dc9e1c77b7a2381fa2a372501d062edd00c581
marriam82 <marriam.s82@gmail.com>
2020-04-28 13:09:03 +0500

    music Album

---
+++ js.js
@@ -0,0 +1,146 @@
+var x = document.getElementById("myAudio"); 
+var y = document.getElementById("myAudio1");
+var z = document.getElementById("myAudio2");
+var a = document.getElementById("myAudio3");
+var b = document.getElementById("myAudio4");
+var c = document.getElementById("myAudio5");
+var d = document.getElementById("myAudio6");
+
+var e = document.getElementById("myAudio7"); 
+var f = document.getElementById("myAudio8");
+var g = document.getElementById("myAudio9");
+var h = document.getElementById("myAudio10");
+var i = document.getElementById("myAudio11");
+var j = document.getElementById("myAudio12");
+var k = document.getElementById("myAudio13");
+
+var l = document.getElementById("myAudio14"); 
+var m = document.getElementById("myAudio15");
+var n = document.getElementById("myAudio16");
+var o = document.getElementById("myAudio17");
+// var abc=0
+//           function playAudio() {
+//             if(abc==0) {
+//             x.play(); 
+//             abc=1
+//             }
+
+//            else if(abc==1) {
+//             x.pause(); 
+//             abc=0
+//             document.getElementById("myAudio").innerHTML=`${<i class='fas fa-pause'></i>}`
+//             }
+//           } 
+//           function pauseAudio() { 
+//             x.pause(); 
+//           } 
+function playAudio() { 
+  x.play(); 
+} 
+function pauseAudio() { 
+  x.pause(); 
+} 
+function playAudio1() { 
+  y.play(); 
+} 
+function pauseAudio1() { 
+  y.pause(); 
+} 
+function playAudio2() { 
+  z.play(); 
+} 
+function pauseAudio2() { 
+  z.pause(); 
+} 
+function playAudio3() { 
+  a.play(); 
+} 
+function pauseAudio3() { 
+  a.pause(); 
+}
+function playAudio4() { 
+  b.play(); 
+} 
+function pauseAudio4() { 
+  b.pause(); 
+}
+function playAudio5() { 
+  c.play(); 
+} 
+function pauseAudio5() { 
+  c.pause(); 
+}
+function playAudio6() { 
+  d.play(); 
+} 
+function pauseAudio6() { 
+  d.pause(); 
+}
+function playAudio7() { 
+  e.play(); 
+} 
+function pauseAudio7() { 
+  e.pause(); 
+}
+function playAudio8() { 
+  f.play(); 
+} 
+function pauseAudio8() { 
+  f.pause(); 
+}
+function playAudio9() { 
+  g.play(); 
+} 
+function pauseAudio9() { 
+  g.pause(); 
+}
+function playAudio10() { 
+  h.play(); 
+} 
+function pauseAudio10() { 
+  h.pause(); 
+}
+function playAudio11() { 
+  i.play(); 
+} 
+function pauseAudio11() { 
+  i.pause(); 
+}
+function playAudio12() { 
+  j.play(); 
+} 
+function pauseAudio12() { 
+  j.pause(); 
+}
+function playAudio13() { 
+  k.play(); 
+} 
+function pauseAudio13() { 
+  k.pause(); 
+}
+function playAudio14() { 
+  l.play(); 
+} 
+function pauseAudio14() { 
+  l.pause(); 
+}
+function playAudio15() { 
+  m.play(); 
+} 
+function pauseAudio15() { 
+  m.pause(); 
+}
+function playAudio16() { 
+  n.play(); 
+} 
+function pauseAudio16() { 
+  n.pause(); 
+}
+function playAudio17() { 
+  o.play(); 
+} 
+function pauseAudio17() { 
+  o.pause(); 
+}
+
+
